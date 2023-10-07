de90204758e215d6bbb557e3999670417f2c1c62
faiaz000 <faiaz000@hotmail.com>
2023-10-07 11:17:48 +0200

    initial commit

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonrepository;
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonrepository = pokemonRepository;
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonrepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemons);
+        }
+
+    }
+}
